9b75015c23474fa2280f41c6118c8019c74e2a4c
Ajay Panwar <ajay.panwar@simubladetechnology.com>
2024-03-19 02:41:39 +0530

    Fixed Error After Resetting Filters

--- src/navigation/screens/Home.tsx
+++ src/navigation/screens/Home.tsx
@@ -5,28 +5,28 @@ import {
   Dimensions,
   FlatList,
   TouchableOpacity,
-} from 'react-native';
-import React, {useEffect, useRef, useState} from 'react';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import {globalStyles} from '../../utils/globalStyles';
-import FlightListCard from '../../components/FlightListCard';
-import {FlightState, HomeScreenProps} from '../../utils/types';
-import {fetchAllFlights} from '../../network';
-import BottomSheet from '@gorhom/bottom-sheet';
-import CustomBottomSheet from '../../components/CustomBottomSheet';
-import {filtersData} from '../../utils/filtersData';
-import Button from '../../components/Button';
+} from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import { globalStyles } from "../../utils/globalStyles";
+import FlightListCard from "../../components/FlightListCard";
+import { FlightState, HomeScreenProps } from "../../utils/types";
+import { fetchAllFlights } from "../../network";
+import BottomSheet from "@gorhom/bottom-sheet";
+import CustomBottomSheet from "../../components/CustomBottomSheet";
+import { filtersData } from "../../utils/filtersData";
+import Button from "../../components/Button";
 
-const Home = ({navigation}: HomeScreenProps) => {
+const Home = ({ navigation }: HomeScreenProps) => {
   // Getting screen dimensions
-  const {width, height} = Dimensions.get('screen');
+  const { width, height } = Dimensions.get("screen");
 
   // State to store flights relate data
   const [flightsList, setFlightsList] = useState<Array<FlightState>>([]);
-  const [sortKey, setSortKey] = useState('asc');
+  const [sortKey, setSortKey] = useState("asc");
   const [selectedFiltersList, setSelectedFilterList] = useState<Array<string>>(
-    [],
+    []
   );
 
   // Ref for bottomsheet
@@ -34,12 +34,20 @@ const Home = ({navigation}: HomeScreenProps) => {
 
   // Function to fetch flights data
   const getFlightsData = () => {
-    fetchAllFlights().then(response => {
-      response?.sort(
-        (flightOne: {price: number}, flightTwo: {price: number}) => {
-          return flightOne?.price - flightTwo?.price;
-        },
-      );
+    fetchAllFlights().then((response) => {
+      if (sortKey === "asc") {
+        response?.sort(
+          (flightOne: { price: number }, flightTwo: { price: number }) => {
+            return flightOne?.price - flightTwo?.price;
+          }
+        );
+      } else {
+        response?.sort(
+          (flightOne: { price: number }, flightTwo: { price: number }) => {
+            return flightTwo?.price - flightOne?.price;
+          }
+        );
+      }
 
       setFlightsList([...response]);
     });
@@ -53,7 +61,7 @@ const Home = ({navigation}: HomeScreenProps) => {
   // Handle filter selection
   const handleFilterSelection = (item: string) => {
     const currentAppliedFilter: Array<string> = [...selectedFiltersList];
-    const foundIndex = currentAppliedFilter.findIndex(filter => {
+    const foundIndex = currentAppliedFilter.findIndex((filter) => {
       return filter === item;
     });
 
@@ -68,7 +76,7 @@ const Home = ({navigation}: HomeScreenProps) => {
 
   // Handle apply filter
   const applyFilter = () => {
-    const filteredFlightList = flightsList?.filter(flight => {
+    const filteredFlightList = flightsList?.filter((flight) => {
       return selectedFiltersList.includes(flight.airline.toLowerCase());
     });
 
@@ -92,10 +100,10 @@ const Home = ({navigation}: HomeScreenProps) => {
   };
 
   // Handle sorting
-  const handleSort = (sortType: 'asc' | 'desc') => {
+  const handleSort = (sortType: "asc" | "desc") => {
     const flightsData = [...flightsList];
 
-    if (sortType === 'asc') {
+    if (sortType === "asc") {
       // Ascending sort
       flightsData?.sort((flightOne, flightTwo) => {
         return flightOne?.price - flightTwo?.price;
@@ -113,7 +121,7 @@ const Home = ({navigation}: HomeScreenProps) => {
 
   return (
     <>
-      <View style={{flex: 1}}>
+      <View style={{ flex: 1 }}>
         {/* Top Container */}
         <View
           style={[
@@ -121,11 +129,12 @@ const Home = ({navigation}: HomeScreenProps) => {
             {
               height: height * 0.4,
             },
-          ]}>
+          ]}
+        >
           {/* Back Button */}
           <Ionicons
             name="chevron-back"
-            color={'#FFF'}
+            color={"#FFF"}
             size={30}
             onPress={() => {
               navigation.goBack();
@@ -138,7 +147,8 @@ const Home = ({navigation}: HomeScreenProps) => {
               {
                 width: width * 0.8,
               },
-            ]}>
+            ]}
+          >
             Book <Text style={styles.secondaryText}>Your</Text>
           </Text>
 
@@ -148,16 +158,17 @@ const Home = ({navigation}: HomeScreenProps) => {
               {
                 width: width * 0.8,
               },
-            ]}>
+            ]}
+          >
             Next Flight
           </Text>
 
           <Ionicons
             name="airplane-outline"
-            color={'#FFF'}
+            color={"#FFF"}
             size={50}
             style={{
-              transform: [{rotate: '-30deg'}],
+              transform: [{ rotate: "-30deg" }],
               marginLeft: width * 0.7,
             }}
           />
@@ -168,9 +179,10 @@ const Home = ({navigation}: HomeScreenProps) => {
           style={[
             styles.flightListContainer,
             {
-              height: '100%',
+              height: "100%",
             },
-          ]}>
+          ]}
+        >
           <Text style={styles.flightListContainerTitle}>
             Select your flight
           </Text>
@@ -180,7 +192,7 @@ const Home = ({navigation}: HomeScreenProps) => {
             {/* Sorting icon */}
             <Ionicons
               name="filter"
-              color={'gray'}
+              color={"gray"}
               size={24}
               onPress={() => bottomSheetRef.current?.expand()}
             />
@@ -189,24 +201,24 @@ const Home = ({navigation}: HomeScreenProps) => {
               {/* For sorting in ascending order */}
               <FontAwesome
                 name="sort-alpha-asc"
-                color={sortKey === 'asc' ? '#000' : 'gray'}
+                color={sortKey === "asc" ? "#000" : "gray"}
                 size={20}
                 onPress={() => {
-                  setSortKey('asc');
-                  handleSort('asc');
+                  setSortKey("asc");
+                  handleSort("asc");
                 }}
               />
 
               {/* For sorting in descending order */}
               <FontAwesome
                 name="sort-alpha-desc"
-                color={sortKey === 'desc' ? '#000' : 'gray'}
+                color={sortKey === "desc" ? "#000" : "gray"}
                 size={20}
                 onPress={() => {
-                  setSortKey('desc');
-                  handleSort('desc');
+                  setSortKey("desc");
+                  handleSort("desc");
                 }}
-                style={{marginLeft: 24}}
+                style={{ marginLeft: 24 }}
               />
             </View>
           </View>
@@ -214,7 +226,7 @@ const Home = ({navigation}: HomeScreenProps) => {
           {/* Flight list */}
           <FlatList
             data={flightsList}
-            style={{marginTop: 12}}
+            style={{ marginTop: 12 }}
             contentContainerStyle={{
               padding: 12,
               gap: 24,
@@ -222,21 +234,21 @@ const Home = ({navigation}: HomeScreenProps) => {
             }}
             showsVerticalScrollIndicator={false}
             keyExtractor={(item, index) => `${item.id} + ${index}}`}
-            renderItem={({item}) => <FlightListCard {...item} />}
+            renderItem={({ item }) => <FlightListCard {...item} />}
           />
         </View>
       </View>
 
       {/* Filter bottom sheet */}
       <CustomBottomSheet sheetRef={bottomSheetRef}>
-        <View style={{flex: 1, padding: 20}}>
+        <View style={{ flex: 1, padding: 20 }}>
           {/* Heading */}
           <View style={styles.filterSheetTitleContainer}>
             <Text style={styles.filterSheetTitle}>Filter By Airlines</Text>
           </View>
 
           {/* Render the filter list */}
-          {filtersData.map(item => {
+          {filtersData.map((item) => {
             return (
               <TouchableOpacity
                 key={item.id + item.airline.toLowerCase()}
@@ -244,14 +256,15 @@ const Home = ({navigation}: HomeScreenProps) => {
                   handleFilterSelection(item.airline.toLowerCase())
                 }
                 activeOpacity={0.6}
-                style={[globalStyles.RSC, styles.checkBoxContainer]}>
+                style={[globalStyles.RSC, styles.checkBoxContainer]}
+              >
                 <Text key={item.id} style={styles.checkboxText}>
                   {item.airline}
                 </Text>
 
                 <View style={styles.checkBoxTickContainer}>
                   {selectedFiltersList.includes(item.airline.toLowerCase()) && (
-                    <FontAwesome name="check" color={'#0067FF'} size={14} />
+                    <FontAwesome name="check" color={"#0067FF"} size={14} />
                   )}
                 </View>
               </TouchableOpacity>
@@ -262,14 +275,14 @@ const Home = ({navigation}: HomeScreenProps) => {
           <Button
             title="Apply"
             handlePress={applyFilter}
-            buttonContainerStyle={{marginTop: 12}}
+            buttonContainerStyle={{ marginTop: 12 }}
           />
           {/* Reset button */}
           <Button
             title="Reset"
             handlePress={resetFilter}
-            buttonContainerStyle={{backgroundColor: '#FDFDFD', marginTop: 24}}
-            titleStyle={{color: '#000'}}
+            buttonContainerStyle={{ backgroundColor: "#FDFDFD", marginTop: 24 }}
+            titleStyle={{ color: "#000" }}
           />
         </View>
       </CustomBottomSheet>
@@ -281,37 +294,37 @@ export default Home;
 
 const styles = StyleSheet.create({
   topContainer: {
-    backgroundColor: '#0067FF',
+    backgroundColor: "#0067FF",
     borderBottomRightRadius: 50,
     borderBottomLeftRadius: 50,
-    justifyContent: 'center',
+    justifyContent: "center",
     paddingHorizontal: 16,
   },
 
   primaryText: {
     fontSize: 40,
     lineHeight: 50,
-    fontWeight: '800',
-    textTransform: 'capitalize',
-    color: '#C2F3A5',
+    fontWeight: "800",
+    textTransform: "capitalize",
+    color: "#C2F3A5",
     marginTop: 24,
   },
 
   secondaryText: {
     fontSize: 36,
     lineHeight: 50,
-    fontWeight: '800',
-    textTransform: 'capitalize',
-    color: '#FFF',
+    fontWeight: "800",
+    textTransform: "capitalize",
+    color: "#FFF",
   },
 
   flightListContainer: {
-    backgroundColor: '#FFF',
+    backgroundColor: "#FFF",
     borderRadius: 20,
     marginHorizontal: 16,
     bottom: 40,
-    shadowColor: '#000',
-    shadowOffset: {width: 2, height: 4},
+    shadowColor: "#000",
+    shadowOffset: { width: 2, height: 4 },
     shadowRadius: 10,
     elevation: 8,
     shadowOpacity: 0.6,
@@ -321,9 +334,9 @@ const styles = StyleSheet.create({
   flightListContainerTitle: {
     fontSize: 20,
     lineHeight: 24,
-    fontWeight: '700',
-    textAlign: 'center',
-    textTransform: 'capitalize',
+    fontWeight: "700",
+    textAlign: "center",
+    textTransform: "capitalize",
   },
 
   filterSortingOptionContainer: {
@@ -340,8 +353,8 @@ const styles = StyleSheet.create({
   filterSheetTitle: {
     fontSize: 24,
     lineHeight: 30,
-    fontWeight: '600',
-    textAlign: 'center',
+    fontWeight: "600",
+    textAlign: "center",
   },
 
   checkBoxContainer: {
@@ -354,7 +367,7 @@ const styles = StyleSheet.create({
   checkboxText: {
     fontSize: 16,
     lineHeight: 18,
-    fontWeight: '500',
+    fontWeight: "500",
   },
 
   checkBoxTickContainer: {
@@ -362,7 +375,7 @@ const styles = StyleSheet.create({
     height: 24,
     borderRadius: 6,
     borderWidth: StyleSheet.hairlineWidth,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
